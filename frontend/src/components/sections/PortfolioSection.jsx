acc55bd677aece0dc0b8e5adaf56cbd9c0c6da6a
rcustom <rcustom@mubas.ac.mw>
2025-12-02 14:27:53 +0200

    portifolio changes

--- frontend/src/components/sections/PortfolioSection.jsx
+++ frontend/src/components/sections/PortfolioSection.jsx
@@ -1,14 +1,16 @@
 const PortfolioSection = () => {
     return (
         <section id="portfolio" className="py-20 bg-bg-secondary">
+             <h2 className="text-4xl font-bold mb-4 text-accent-biege container mx-auto px-4 flex flex-row gap-2 items-center">
+  About <span className="text-sky-600">Our Innovation Hub</span>
+</h2>
+
             <div className="container mx-auto px-4 flex flex-col md:flex-row gap-16 items-center">
                 <div className="flex-1">
                     <img src="/images/background.jpg" alt="Innovation Hub Team" className="w-full h-auto rounded-2xl shadow-lg" />
                 </div>
                 <div className="flex-1">
-                    <h2 className="text-4xl font-bold mb-4 text-accent-biege">
-                        About <span className="text-text-dark">Our Innovation Hub</span>
-                    </h2>
+                   
                     <p className="text-text-secondary mb-8 text-lg">
                         We are a community-driven Innovation Hub empowering students, entrepreneurs, and creators to bring their ideas to life.
                         Through access to modern facilities, mentorship, and collaborative spaces, we nurture innovation that drives real-world change.
@@ -27,10 +29,13 @@ const PortfolioSection = () => {
                             </p>
                         </div>
                     </div>
-
-                    <div className="mt-8">
-                        <h3 className="text-xl font-semibold mb-2 text-accent-biege">What Sets Us Apart</h3>
-                        <ul className="text-text-secondary mb-6 list-disc list-inside text-base space-y-2">
+                </div>
+                
+            </div>
+            <div className="container mt-5 mx-auto px-4 flex flex-col md:flex-row gap-16 items-center">
+                        
+                        <ul className="text-text-secondary mb-6 list-none list-inside text-base space-y-2">
+                            <h3 className="text-xl font-semibold mb-2 text-accent-biege">What Sets Us Apart</h3>
                             <li><i className="fa fa-rocket mr-2 text-accent-green"></i> State-of-the-art innovation labs and co-working spaces</li>
                             <li><i className="fa-solid fa-handshake mr-2 text-accent-green"></i> Strong network of mentors, experts, and investors</li>
                             <li><i className="fa-solid fa-lightbulb mr-2 text-accent-green"></i> Hands-on learning through workshops and hackathons</li>
@@ -40,8 +45,6 @@ const PortfolioSection = () => {
                             Learn More <i className="fas fa-arrow-right ml-2"></i>
                         </a>
                     </div>
-                </div>
-            </div>
         </section>
     );
 };
